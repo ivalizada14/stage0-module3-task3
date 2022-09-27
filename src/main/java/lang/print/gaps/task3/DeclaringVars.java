57f9f686fb731a3fab2efc429e893a88af556422
Ibrahim Velizade <ivalizada14@gmail.com>
2022-09-27 17:15:07 +0300

    forth attempt

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,7 +2,9 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a = 10, b = 10, c = 10;
+        int a = 10;
+        int b = 10;
+        int c = 10;
         System.out.println(c);
 
     }
